1e2959e0523fa15e42a2737ac38faafd9cc111ed
pixelius <pixelius@users.noreply.github.com>
2019-07-10 18:42:28 +0200

    Initial commit

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+    <main class="main">
+
+		<span class="php">404.php</span>
+
+		<h1><?php wp_title( '' ); ?></h1>
+		<p>helpful advice</p>
+	
+	</main>
+
+<?php get_footer(); ?>
