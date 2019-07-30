9a491391b82450df954a33d494abb6eb0df774d5
pixelius <pixelius@users.noreply.github.com>
2019-07-30 17:26:26 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+
+    <span class="php">footer.php</span>
+
+</footer>
+
+</div>
+
+<?php wp_footer();?>
+</body>
+</html>
---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+<main class="main">
+
+    <span class="php">front-page.php</span>
+
+    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+
+        <h1><?php the_title();?></h1>
+        <hr>
+        <?php the_content();?>
+
+    <?php endwhile; endif;?>    
+
+</main>
+
+<?php get_footer();?>
