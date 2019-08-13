247f28b9f565704fd6bb2d486c2c0a369c0448a5
pixelius <pixelius@users.noreply.github.com>
2019-08-13 20:27:17 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     alert('JavaScript loaded - DOM fully loaded and parsed');
+//      console.log("DOM fully loaded and parsed");
+//  }); 
